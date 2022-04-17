601120340b95aaf123dfde2a15894b394ef295df
Dare Sodiq <daresodiq000@gmail.com>
2022-04-17 14:22:28 +0100

    Update utils.go

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -8,14 +8,6 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func ParseBody(r *http.Request, x interface{}) {
-	if body, err := ioutil.ReadAll(r.Body); err == nil {
-		if err := json.Unmarshal([]byte(body), x); err != nil {
-			return
-		}
-	}
-}
-
 func Hash(passsword string) string {
 	password := []byte("MyDarkSecret")
 	pass, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
