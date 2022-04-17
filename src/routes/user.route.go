cc3e422fc59915f4e65e7fb5493656d8bf57e30b
Dare <daresodiq000@gmail.com>
2022-04-17 15:03:36 +0100

    handler

--- src/routes/user.route.go
+++ src/routes/user.route.go
@@ -2,10 +2,10 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/horlaarsco/bank/src/controllers"
+	"github.com/horlaarsco/bank/src/handlers"
 )
 
 func UserRoutes(mux *mux.Router) {
-	mux.HandleFunc("/user", controllers.GetUser).Methods("GET")
-	mux.HandleFunc("/user", controllers.CreateUser).Methods("POST")
+	mux.HandleFunc("/user", handlers.GetUser).Methods("GET")
+	mux.HandleFunc("/user", handlers.CreateUser).Methods("POST")
 }
--- src/types/user.type.go
+++
@@ -1,8 +0,0 @@
-package types
-
-type IUser struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-}
